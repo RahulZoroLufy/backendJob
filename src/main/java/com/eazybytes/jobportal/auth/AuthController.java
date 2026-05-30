2e78e8f6cc9ba4d444b3fcdfe35be4c1fafb18d4
Rahul <rahulb.tech1999@gmail.com>
2026-05-30 14:56:50 +0530

    changes till here i have implemented till jwt token is retuned when you login

--- src/main/java/com/eazybytes/jobportal/auth/AuthController.java
+++ src/main/java/com/eazybytes/jobportal/auth/AuthController.java
@@ -26,6 +26,7 @@ public class AuthController {
 
     @PostMapping(value = "/login/public",version = "1.0")
     public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody LoginRequestDto loginRequestDto) {
+
         try {
             var resultAuthentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequestDto.username(),
                     loginRequestDto.password()));
--- src/main/java/com/eazybytes/jobportal/security/util/JwtUtil.java
+++ src/main/java/com/eazybytes/jobportal/security/util/JwtUtil.java
@@ -23,14 +23,13 @@ public class JwtUtil {
 
     public String generateJwtToken(Authentication authentication){
         String jwtToken;
+
         String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY,
                 ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
         SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-        var fetchedUser = (JobPortalUser) authentication.getPrincipal();
+        var fetchedUser = (User) authentication.getPrincipal();
         jwtToken = Jwts.builder().issuer("Job Portal").subject("JWT Token")
-                .claim("name", fetchedUser.getName())
-                .claim("email", fetchedUser.getEmail())
-                .claim("mobileNumber", fetchedUser.getMobileNumber())
+                .claim("name", fetchedUser.getUsername())
                 .claim("roles", authentication.getAuthorities().stream().map(
                         GrantedAuthority::getAuthority).collect(Collectors.joining(",")))
                 .issuedAt(new java.util.Date())
